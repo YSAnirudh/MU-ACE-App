231bfb2276cdeef8dc2a046c99299f6ccf4dfd5f
Rohan Jo <rohan54518@gmail.com>
2021-04-22 09:27:08 +0530

    Login Page done

---
+++ MyApp/components/LoginTxtF.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import {TextInput,View,StyleSheet} from 'react-native'
+import AntDesign from 'react-native-vector-icons/AntDesign'
+import Colors from '../constants/Colors'
+import { screenHeight,screenWidth } from '../utils/ScreenParams';
+
+const LoginTxtF=({label,placeholder,icon,...other})=>{
+    return(    
+        <View style={styles.TF}>
+            <View style={styles.iconStyle}>
+                <AntDesign name={icon} size={25} color={Colors.primary_light}/>
+            </View>    
+            <TextInput label={label} numberOfLines={1} placeholder={placeholder} placeholderTextColor='#666' style={styles.input} required {...other}>
+            </TextInput>      
+        </View>
+    );
+}
+
+const styles =StyleSheet.create({
+    TF:{
+        marginTop:5,
+        marginBottom:10,
+        width:'100%',
+        height:screenHeight/15,
+        borderColor:Colors.secondary,
+        borderRadius:20,
+        borderWidth:1,
+        flexDirection:'row',
+        alignItems:'center',
+        backgroundColor:Colors.secondary_dark
+    },
+    iconStyle:{
+        padding:10,
+        height:'100%',
+        justifyContent:'center',
+        alignItems:'center',
+        borderRightColor:Colors.secondary,
+        color:Colors.secondary,
+        borderRightWidth:1,
+        width:50
+    },
+    inputField:{
+        padding:10,
+        marginTop:5,
+        marginBottom:10,
+        width:screenWidth/1.5,
+        height:screenHeight/15,
+        fontSize:16,
+        borderRadius:8,
+        borderWidth:1
+
+
+
+    },
+    input:{
+        padding:10,
+        flex:1,
+        fontSize:16,
+        color:Colors.secondary,
+        justifyContent:'center',
+        alignItems:'center'
+    }
+
+});
+export default LoginTxtF
+
---
+++ MyApp/components/OutlookBtn.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {Text,StyleSheet,TouchableOpacity,View} from 'react-native'
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { screenHeight,screenWidth } from '../utils/ScreenParams';
+
+const OutlookBtn=({bname,type,color,backgroundColor,...other})=>{
+    var bgColor=backgroundColor
+    return( 
+
+        <TouchableOpacity style={[styles.buttonStyle,{backgroundColor:bgColor}]} {...other}>
+            <View style={styles.wrapIcon}>
+                <MaterialCommunityIcons name={type} style={styles.icon} size={24} color="black" />
+                
+            </View>
+            <View style={styles.buttonTextWrapper}>
+                <Text style={[styles.buttonText,{color:color}]}>{bname}</Text>
+            </View>
+            
+        </TouchableOpacity>
+    );
+
+}
+
+const styles=StyleSheet.create({
+    buttonStyle:{
+        marginTop:1,
+        width:'100%',
+        height:screenHeight/15,
+        padding:10,
+        flexDirection:'row',
+        borderRadius:20
+    },
+    buttonText:{
+        fontSize:18,
+        
+        
+
+    },
+    icon:{
+        fontWeight:'bold'
+    },
+    wrapIcon:{
+        width:30,
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    buttonTextWrapper:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center'
+    }
+
+});
+
+export default OutlookBtn
