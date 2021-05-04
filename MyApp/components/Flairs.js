d1d3d6b0b98a8b76229f84b8d7e9d43fb8bb6b62
Rohan Jo <rohan54518@gmail.com>
2021-05-05 00:13:03 +0530

    create Post Screen Done

---
+++ MyApp/components/Flairs.js
@@ -0,0 +1,106 @@
+import React, { Component,useState } from 'react';
+import { View,SafeAreaView, StyleSheet, Text } from 'react-native';
+import MultiSelect from 'react-native-multiple-select';
+import Colors from '../constants/Colors'
+import { screenHeight } from '../utils/ScreenParams';
+
+const items = [{
+    id: '1',
+    name: 'CS'
+  }, {
+    id: '2',
+    name: 'M E'
+  }, {
+    id: '3',
+    name: 'EE'
+  }, {
+    id: '4',
+    name: 'CE'
+  }, {
+    id: '5',
+    name: 'H S'
+  }, {
+    id: '6',
+    name: 'Math'
+  }, {
+    id: '7',
+    name: 'Phy'
+  }, {
+    id: '8',
+    name: 'Chem'
+  }, {
+    id: '9',
+    name: 'Arts'
+    }
+];
+
+const Flairs=()=>{
+   // Data Source for the SearchableDropdown
+  const [selectedItems, setSelectedItems] = useState([]);
+
+  const onSelectedItemsChange = (selectedItems) => {
+    // Set Selected Items
+    setSelectedItems(selectedItems);
+  };
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <Text style={styles.titleText}>
+          Flairs
+        </Text>
+        <MultiSelect
+          hideTags
+          items={items}
+          uniqueKey="id"
+          onSelectedItemsChange={onSelectedItemsChange}
+          selectedItems={selectedItems}
+          selectText="Choose..."
+          searchInputPlaceholderText="Search..."
+          onChangeInput={(text) => console.log(text)}
+          tagRemoveIconColor={Colors.Gold}
+          tagBorderColor={Colors.Gold}
+          tagTextColor="blue"
+          selectedItemTextColor={Colors.Gold}
+          selectedItemIconColor={Colors.Gold}
+          itemTextColor={Colors.Red}
+          displayKey="name"
+          searchInputStyle={{color: Colors.DarkGrey}}
+          hideSubmitButton
+          fixedHeight
+          searchInputStyle={{ color: Colors.DarkGrey }}
+          styleDropdownMenuSubsection={styles.dropMenu} 	
+          styleItemsContainer={styles.dropMenu}
+          styleRowList={styles.dropMenu}
+         
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor:Colors.Grey ,
+      padding: 10,
+      width:'100%',
+      zIndex:10
+     
+    },
+    titleText: {
+      padding: 8,
+      fontSize: 16,
+      textAlign: 'center',
+      fontWeight: 'bold',
+      color:Colors.Gold
+    },
+    headingText: {
+      padding: 8,
+    },
+    dropMenu:{
+        backgroundColor:Colors.Grey,
+       
+    }
+  });
+
+export default Flairs;
